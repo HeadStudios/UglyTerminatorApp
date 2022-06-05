548d0c9f1076cf4e92a246136f5cf3a6b0255d55
HeadStudios <kosta@headstudios.com.au>
2022-06-05 18:27:13 +1000

    Update to latest version
    Just updating a few things - getting everything ready for prime time and sync.. oh and Synthesia works too

--- the_vision.php
+++ the_vision.php
@@ -2,6 +2,7 @@
 
 require_once("wp-load.php"); // Make sure there are enough dot dot dots
 require("connects.php");
+require("env.php"); // this is in Gitignore for security reasons, ask me for access and I can provide
 require("parsedown.php"); // This is the convert AirTable rich text field to HTML to pass to new WP post function
 
 /* In the section below I comment the line for file_get_contents if I am testing so I could supply my own data and check output to speed up development. Makes sense?
@@ -10,10 +11,10 @@ const data = { title: blogTitle, function: 1, excerpt: 'The short version', cont
 Also this is where I will define all the variables to be used 
 */
 // Comment out the 3 lines below and uncomment everything underneath when doing testing directly at endpoint (vs sending from AirTable)
-$data = json_decode(file_get_contents('php://input'), true);
+//$data = json_decode(file_get_contents('php://input'), true);
 
-//$temp_data = '{"title":"Brian Gumble","function":1,"excerpt":"The short version","content":"This was generated with a beautiful shortlink. \n\nIt\'s because the woods are scary.\n\nThankfully - now you have... CATARACTS!\n\nWith cataracts you will be\n\n- Scared all the time\n- Never know what to do\n- Always try to find your way\n\nCataracts - sold where all goods are sold! \n","bannerhead":"Amazing Offer","headline":"Cataract Appointments Available","subheadline":"Your Doctor Will See You Now","cta_text":"Cataract Booking Form","cta_link":"https://airtable.com","short_path":"miracles-of-love5"}';
-//$data = json_decode($temp_data, true);
+$temp_data = '{"title":"John Connor Returns","name":"Sarah Connor","function":1,"excerpt":"The short version","content":"This was generated with a beautiful shortlink. \n\nIt\'s because the woods are scary.\n\nThankfully - now you have... CATARACTS!\n\nWith cataracts you will be\n\n- Scared all the time\n- Never know what to do\n- Always try to find your way\n\nCataracts - sold where all **goods are sold!** \n","bannerhead":"Amazing Offer","headline":"Cataract Appointments Available","subheadline":"Your Doctor Will See You Now","cta_text":"Book your Cataracts Form","cta_link":"https://airtable.com","short_path":"for-you-michael-160"}';
+$data = json_decode($temp_data, true);
 
 
 // Below is just some code because I've noticed AirTable rich text formatting doesn't work if I have custom code - I need to test to see if I can remove this and have it still work
@@ -48,13 +49,16 @@ if($data["function"] == 2){
 $arr = array('server' => 'And this is the second', 'ashram' => $data["title"], 'hilltop' => 'Let us begin', 'machine' => 'And dance to dancers', 'array' => 'Unless we are human');
 } 
 
+$ai_vid = robotUprising($data['name']);
+
 $wp_custom = array (
     'fields' => array (
         'bannerhead' => $data["bannerhead"],
         'headline' => $data["headline"],
         'subheadline' => $data["subheadline"],
         'cta_text' => $data["cta_text"],
-        'cta_link' => $data["cta_link"]
+        'cta_link' => $data["cta_link"],
+        'synthesia_id' => $ai_vid
 
     )
 ); 
@@ -67,12 +71,29 @@ at this point. But basically
 */
 
 
+// Note in this section I comment out bannerCreate and set uid directly to avoid API data limits (as Bannerbear only allows like 30 API calls on the trial)
+//$uid = bannerCreate($data["name"]);
+//$uid = bannerCreate($data["name"]);
+$uid = '9e2VGL0qn6VVnr4m6EAv5mxr1';
 $newPost = newCustomPost($data["title"], $content); 
 $newPostResults = json_decode($newPost, true);
 $arr['url'] = $newPostResults['guid']['rendered'];
 $id = $newPostResults['id'];
 customFieldPush($id, $wp_custom);
-$arr['shortlink'] = short_It($arr['url'], $data['short_path']);
+
+// Commenting out banner creation to save on API calls
+
+//$uid = '9e2VGL0qn6VVnr4m6EAv5mxr1';
+
+
+//$arr['shortlink'] = short_It($arr['url'], $data['short_path']);
+$link_result = short_it($arr['url'], $data['short_path']);
+$arr['shortlink'] = $link_result['shortURL'];
+$image_url = getBearImage($uid);
+echo "And we have an Image URL field and it is: ".$image_url." do you see it?";
+OGLinkStatus($link_result['idString'], $data['name'], $data['excerpt'], $image_url);
+
+
 
 //$arr = newCustomPost($data["title"]);
 //$arr = json_decode($arr, true);
@@ -81,3 +102,7 @@ $arr['shortlink'] = short_It($arr['url'], $data['short_path']);
 
 
 echo json_encode($arr);
+
+
+
+
